e3a90b7123aafed8d07e3109da63b96b63577b6f
joingithubopensource <sreebuvanes@gmail.com>
2023-07-30 09:25:28 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './login/login.component';
+import {FormComponent} from './form/form.component';
 
 const routes: Routes = [
-    {path:'',pathMatch:'full',redirectTo:'login'},
-  {path:'login',component:LoginComponent},
+    {path:'',pathMatch:'full',redirectTo:'form'},
+  {path:'form',component:FormComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,22 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DemoMaterialModule } from './demo-material/demo-material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    FormComponent,
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
     DemoMaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
